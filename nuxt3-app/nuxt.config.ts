dfb1a51a96542478679805c12ec5c97b2c003189
onishi.kohei <kohei.o2222@gmail.com>
2022-02-27 23:10:33 +0900

    vuetify

--- nuxt3-app/nuxt.config.ts
+++ nuxt3-app/nuxt.config.ts
@@ -1,6 +1,8 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+      transpile: ['vuetify']
+  },
 })
