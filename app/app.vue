bed1231f8cc79a16dd466c61badbdbfd8b0214df
onishi.kohei <kohei.o2222@gmail.com>
2022-03-06 22:29:26 +0900

    meta追加

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <Html lang="ja">
+      <Head>
+        <Title>{{ title }}</Title>
+        <Meta name="description" :content="`This is ${title} page`" />
+      </Head>
+    </Html>
     <NuxtPage />
   </div>
 </template>
+<script setup lang="ts">
+const title = 'Hello Nuxt3!!'
+</script>
