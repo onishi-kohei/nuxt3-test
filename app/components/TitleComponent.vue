97b0f94bd51b34749efaaa820e70866c44927491
onishi.kohei <kohei.o2222@gmail.com>
2022-03-06 20:49:45 +0900

    eslint設定追加

--- app/components/TitleComponent.vue
+++ app/components/TitleComponent.vue
@@ -1,9 +1,6 @@
 <template>
-    <div>
-        <h2>COMPONENT</h2>
-        <v-btn
-        elevation="2"
-        outlined
-        >button</v-btn>
-    </div>
+  <div>
+    <h2>COMPONENT</h2>
+    <v-btn elevation="2" outlined>button</v-btn>
+  </div>
 </template>
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   css: ['vuetify/lib/styles/main.sass', '@/assets/css/tailwind.css'],
   buildModules: ['@nuxtjs/tailwindcss'],
   build: {
-      transpile: ['vuetify']
+    transpile: ['vuetify'],
+  },
+  typescript: {
+    strict: true,
   },
 })
