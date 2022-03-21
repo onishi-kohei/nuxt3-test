d2c8e5b42ca3058938dead1d9c16784162971dc1
onishi.kohei <kohei.o2222@gmail.com>
2022-03-21 22:21:22 +0900

    TitleComponent削除

--- app/components/TitleComponent.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h2>COMPONENT</h2>
-    <v-btn elevation="2" outlined>button</v-btn>
-  </div>
-</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,5 @@
   <div>
     <h1>INDEX</h1>
     <div class="text-red-700">test</div>
-    <TitleComponent />
   </div>
 </template>
