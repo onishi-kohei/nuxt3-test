5c5c75906c4f99fdb022c89e3cadfa5d559fdf73
onishi-kohei <kohei.o2222@gmail.com>
2023-01-04 17:28:49 +0900

    setup の練習

--- app/components/TitleComponent.vue
+++ app/components/TitleComponent.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <h2>COMPONENT</h2>
+    <h2>double</h2>
+    <div>{{ double }}</div>
+    <h2>triple</h2>
+    <div>{{ triple }}</div>
     <v-btn elevation="2" outlined>button</v-btn>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  async setup() {
+    const count = ref(2)
+    const triple = computed(() => count.value * 3)
+    return {
+      count,
+      triple
+    }
+  },
+  computed: {
+    double() {
+      return this.count * 2
+    }
+  }
+})
+</script>
