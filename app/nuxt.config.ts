3784d954e927d3a1bc9fb63bb5ced28ba21a1c4b
onishi-kohei <kohei.o2222@gmail.com>
2023-01-04 16:52:19 +0900

    nuxt3のアップデート

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,3 @@
-import { defineNuxtConfig } from 'nuxt3'
-
 export default defineNuxtConfig({
   css: ['vuetify/lib/styles/main.sass', '@/assets/css/tailwind.css'],
   buildModules: ['@nuxtjs/tailwindcss'],
